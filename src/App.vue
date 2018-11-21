1aec3bcf017acce369c2cc0729247d1b4f7f4039
Ezra Obiwale <contact@ezraobiwale.com>
2018-11-21 09:42:29 +0100

    Done

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,74 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <basic-chat 
+      title="Dev Center"
+      :new-message="message">
+      <div slot="input-container" class="input__container">
+        <div class="input__field">
+          <input
+            type="text"
+            name="message"
+            aria-placeholder="Type message..."
+            placeholder="Type message..."
+            autofocus
+            v-model="content"
+            @keyup.enter.prevent="send"><br>
+        </div>
+        <div
+          class="input__button"
+          @click="send">
+          Send
+        </div>
+      </div>
+    </basic-chat>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BasicChat from 'basic-vue-chat'
+import moment from 'moment'
+import axios from 'axios'
 
 export default {
   name: 'app',
+  data () {
+    return {
+      content: '/giphy laugh',
+      lastId: 0,
+      message: {}
+    }
+  },
   components: {
-    HelloWorld
+    BasicChat
+  },
+  methods: {
+    send () {
+      let content = this.content.trim()
+      this.content = ''
+      if (content.startsWith('/giphy')) {
+        axios.get('https://api.giphy.com/v1/gifs/translate?api_key=nNxG4vhbZJTkurhUwLtKa8Lj8XwjAeQv&s=' + content + '&wierdness=' + this.wierdness())
+          .then(resp => {
+            this.updateMessage(resp.data.data.images.downsized_medium.url, true)
+
+          })
+          .catch(resp => {
+            alert('something went wrong', resp)
+          })
+      } else {
+        this.updateMessage(content)
+      }
+    },
+    updateMessage (content, isImage) {
+      this.$set(this, 'message', {
+        id: 0,
+        imageUrl: isImage ? content : '',
+        contents: content,
+        date: moment().format('H:m:s')
+      })
+    },
+    wierdness () {
+      return Math.round(Math.random(10) * 10)
+    }
   }
 }
 </script>
