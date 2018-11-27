1e63d59f2d8075c609ecb92d65578b2a4cac7953
Ezra Obiwale <contact@ezraobiwale.com>
2018-11-27 15:09:47 +0100

    Removed server test utils reference

--- tests/unit/chat.spec.js
+++ tests/unit/chat.spec.js
@@ -1,5 +1,4 @@
 import { mount, createLocalVue } from '@vue/test-utils'
-import { render } from '@vue/server-test-utils'
 import Chat from '@/views/Chat.vue'
 import router from '@/router'
 import store from '@/store'
@@ -9,7 +8,7 @@ const localVue = createLocalVue()
 localVue.use(Vuex)
 
 describe('Chat page', () => {
-  let wrapper, rendered, input
+  let wrapper
 
   beforeEach(() => {
     wrapper = mount(Chat, {
@@ -17,17 +16,7 @@ describe('Chat page', () => {
       router,
       store
     })
-    rendered = render(Chat, {
-      localVue,
-      router,
-      store
-    })
     wrapper.vm.setUsername('alex')
-    input = rendered.find('input')
-  })
-
-  it('should contain an input field', () => {
-    expect(input.exists()).toBe(true)
   })
 
   it('should have username as alex', () => {
@@ -37,22 +26,5 @@ describe('Chat page', () => {
   it('should have no messages', () => {
     expect(wrapper.vm.messages).toHaveLength(0)
   })
-
-  // it('should increment messages when saveMessage is called', () => {
-  //   wrapper.vm.saveMessage('First Message')
-  //   expect(wrapper.vm.messages).toHaveLength(1)
-  // })
-
-  // it('should increment messages by one if message is set and send method is called', () => {
-  //   wrapper.vm.message = 'Test'
-  //   expect(wrapper.vm.message).toBe('Test')
-  //   wrapper.vm.send()
-  //   expect(wrapper.vm.messages).toHaveLength(1)
-  // })
-
-  // it('should clear username when logout is clicked', () => {
-  //   wrapper.vm.logout()
-  //   expect(wrapper.vm.username).toBe('')
-  // })
 
 })
